78b11f8e09c27fabb678235ed09a0bcc9340cee9
Brandon Pulera <87675283+Pulerabj11@users.noreply.github.com>
2022-07-05 15:43:57 -0500

    Got search from typesense working

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,4 @@
-const functions = require("firebase-functions");
+const typesense = require("typesense")
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,3 +7,25 @@ const functions = require("firebase-functions");
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+async function search() {
+    const client = new typesense.Client({
+        'nodes': [{
+            'host': 'p10ykefx4hnjsu72p-1.a1.typesense.net', // where xxx is the ClusterID of your Typesense Cloud cluster
+            'port': '443',
+            'protocol': 'https'
+        }],
+        'apiKey': '5tUAfECHqfVgKuVOTGsfB5z5RW4j6u65',
+        'connectionTimeoutSeconds': 2
+    });
+
+    let searchParameters = {
+        'q': 'bloodbone',
+        'query_by': 'Lore, Name'
+    }
+
+    results = await client.collections('Ammunition').documents().search(searchParameters)
+
+    console.log(results.hits[0].document.Name)
+}
+
+search()
