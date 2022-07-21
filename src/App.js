acd77483afc71537bdb9b4d8f46cdae4a6aa23e4
Brandon Pulera <pulerabj11@gmail.com>
2022-07-20 20:55:32 -0500

    Trying to get JQuery working

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import './App.css'
+
+import { useEffect } from 'react'
+
 // Components Imports
 import SearchBar from './Components/SearchBar'
 
 function App() {
+
+  // useEffect(() => {
+
+  //   console.log(itemLore)
+    
+  //   for (var i = 0; i < itemLore.length; i++) {
+  //     console.log(itemLore[i])
+  //     // if (itemLore !== undefined) {
+
+  //     //   // g flag looks for all matches, not just first
+  //     //   // i flag is case-insensitive
+  //     //   const regex = new RegExp('blood', 'gi')
+
+  //     //   var text = itemLore.innerHTML
+
+  //     //   // $& is a replacement patter that tells the replacer method to insert the matched substring there
+  //     //   const newText = text.replace(regex, "<mark className='highlight'>$&</mark>")
+  //     //   itemLore.innerHTML = newText
+  //     // }
+  //   }
+  // })
 
   return (
     <div className='container'>
